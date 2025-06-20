e0d8b0a3cb863570e574b904aa0e8b319ba38fd5
Yusuke Nakamura <yu9su1ke1@gmail.com>
2025-06-20 07:13:51 +0500

    Fix: add actual files instead of submodule

---
+++ MatchingWithSwiftUI2/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  MatchingWithSwiftUI
+//
+//  Created by 中村優介 on 2025/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ListView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ MatchingWithSwiftUI2/MatchingWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingWithSwiftUIApp.swift
+//  MatchingWithSwiftUI
+//
+//  Created by 中村優介 on 2025/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchingWithSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
